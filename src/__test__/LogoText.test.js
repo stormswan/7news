c77d2542135a83eeb2f6a8c0159c17beffc1edd0
Bob <bobhuipeng@gmail.com>
2018-08-11 16:20:43 +1000

    logo text size change in different screen size

--- src/__test__/LogoText.test.js
+++ src/__test__/LogoText.test.js
@@ -9,3 +9,33 @@ test('Shapshot', () => {
   const logoText = renderer.create( <LogoText>Home and Away</LogoText>).toJSON()
   expect(logoText).toMatchSnapshot()
 })
+
+test('should change font size at differnet screen size', () => {
+  const logoText = renderer.create( <LogoText>Home and Away</LogoText>).toJSON()
+  expect(logoText).toHaveStyleRule('font-size', '4em', {
+    media: '(min-width: 992px)'
+  })
+
+  expect(logoText).toHaveStyleRule('font-size', '5em', {
+    media: '(min-width: 1200px)'
+  })
+
+  expect(logoText).toHaveStyleRule('font-size', '1.8em', {
+    media: '(max-width: 576px)'
+  })
+});
+
+
+test('should work with default value', () => {
+  const logoText = renderer.create( <LogoText>Home and Away</LogoText>).toJSON()
+  expect(logoText).toHaveStyleRule('color','white')
+  expect(logoText).toHaveStyleRule('font-size','3em')
+});
+
+test('should work with props value', () => {
+  const color = 'green'
+  const size ='2em'
+  const logoText = renderer.create( <LogoText color={color} size={size}>Home and Away</LogoText>).toJSON()
+  expect(logoText).toHaveStyleRule('color',color)
+  expect(logoText).toHaveStyleRule('font-size',size)
+});
--- src/components/LogoText.jsx
+++ src/components/LogoText.jsx
@@ -1,8 +1,23 @@
 import styled from 'styled-components'
 
-const LogoText = styled.div`
-    color: white;
-    font-size: 2em;
+const LogoText = styled.div.attrs ({
+  size: props => props.size || '3em',
+  color: props => props.color || 'white',
+})`
+    color: ${props => props.color};
+    font-size: ${props => props.size};
+
+    @media (max-width: 576px) {
+        font-size: 1.8em;
+    };
+
+    @media (min-width: 992px) {
+        font-size: 4em;
+    };
+
+    @media (min-width: 1200px) {
+        font-size: 5em;
+    };
 `
 
 export default LogoText
