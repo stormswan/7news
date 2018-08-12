aa4f02326935f43f3522500097e4f54f601b2de4
Bob <bobhuipeng@gmail.com>
2018-08-12 11:43:08 +1000

    add more test case and alt for title background image

--- src/__test__/BackgroundImg.test.js
+++ src/__test__/BackgroundImg.test.js
@@ -11,3 +11,17 @@ test('Shapshot', () => {
   expect(titleBgImg).toMatchSnapshot()
 })
 
+
+test('should work with default value', () => {
+  const backgroundImg = renderer.create( <TitleBackgroundImg/>).toJSON()
+  expect(backgroundImg).toHaveStyleRule('width','100%')
+  expect(backgroundImg.props.alt).toBe('title background image');
+});
+
+test('should work with props value', () => {
+  const alt= 'background'
+  const src = '../images/tile.jpg';
+  const backgroundImg = renderer.create( <TitleBackgroundImg alt={alt} src={src}/>).toJSON()
+  expect(backgroundImg.props.alt).toBe(alt);
+  expect(backgroundImg.props.src).toBe(src);
+});
--- src/components/TitleBackgroundImg.jsx
+++ src/components/TitleBackgroundImg.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 const TitleBackgroundImg = styled.img.attrs ({
   src: props => props.src,
+  alt: props => props.alt || 'title background image',
 })`
   width:100%;
 `
